2af05f25950cbd140adb4edb2e502161583735de
Cee <hungns126@gmail.com>
2021-03-02 21:34:41 +0700

    add layout

--- src/components/screens/event/index.js
+++ src/components/screens/event/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Event = () => {
   return (
     <div>
-      {Array(50).fill(null).map((e, i) => <h1>Event</h1>)}
+      {Array(100).fill(null).map((e, i) => <h1>Event</h1>)}
     </div>
   );
 }
